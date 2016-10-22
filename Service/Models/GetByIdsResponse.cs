797c1d14d7da0b96c534f1fc069d7943e7c149b5
dan nguyen <ddnguyen0702@hotmail.com>
2016-10-22 20:59:46 +0700

    update

---
+++ Service/Models/GetByIdsResponse.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Service.Models
+{
+    public class GetByIdsResponse
+    {
+        public List<StockDetails> Stocks { get; set; }
+        public int Page { get; set; }
+        public int Size { get; set; }
+        public int Count { get; set; }
+    }
+}
