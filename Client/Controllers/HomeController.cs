07b13d988b635403b8023fb112d60b7428910a25
dan nguyen <ddnguyen0702@hotmail.com>
2016-10-22 22:06:33 +0700

    update

--- Client/Controllers/HomeController.cs
+++ Client/Controllers/HomeController.cs
@@ -6,13 +6,14 @@ using System.Web;
 using System.Web.Mvc;
 using Client.Models;
 using Client.ServiceAgents;
+using Common;
 using X.PagedList;
 
 namespace Client.Controllers
 {
     public class HomeController : Controller
     {
-        private IStockService _stockService;
+        private readonly IStockService _stockService;
 
         public HomeController(IStockService stockService)
         {
@@ -25,6 +26,14 @@ namespace Client.Controllers
             var listItems = stocks.Stocks.Select(s => new StockListItem() {Id = s.Id, Price = s.Price});
             var vm = new StaticPagedList<StockListItem>(listItems, page, size, stocks.Count);
             return View(vm);
+        }
+
+        public ActionResult Refresh(int page, int size)
+        {
+            var stocks = AsyncHelper.RunSync(() => _stockService.GetAllAsync(page, size));
+            var listItems = stocks.Stocks.Select(s => new StockListItem() { Id = s.Id, Price = s.Price });
+            var vm = new StaticPagedList<StockListItem>(listItems, page, size, stocks.Count);
+            return PartialView(vm);
         }
     }
 }
